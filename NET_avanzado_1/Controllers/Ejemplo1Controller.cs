20c1ed7333313efa02db94e27cba75d2c04b50ef
rusokverse <rusokverse@MACBOOKPRO>
2022-10-24 19:59:16 -0400

    Agregar archivos de proyecto.

---
+++ NET_avanzado_1/Controllers/Ejemplo1Controller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NET_avanzado_1.Controllers
+{
+    [Route("api/ejemplo1")]
+    public class Ejemplo1Controller : Controller
+    {
+        [HttpGet("prueba1")]
+        public async Task<IActionResult> prueba1()
+        {
+            try {
+                return Ok("Esta es una prueba");
+            }
+            catch {
+                return BadRequest();
+            }
+        }
+        [Produces("text/html")]
+        [HttpGet("prueba2")]
+        public async Task<IActionResult> prueba2()
+        {
+            try
+            {
+                return Ok("<h2><b>Esta es una prueba</b></h2>");
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet("prueba3/{val}")]
+        public async Task<IActionResult> prueba3(string val)
+        {
+            try
+            {
+                return Ok("<h2><b>Buenas noches señor "+ val +"</b></h2>");
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
